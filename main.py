c777fac4286d9a645e7e77b15500da4d66af2dfe
forana <alex@alexforan.com>
2021-06-22 15:19:57 -0500

    fix bug, switch to mongo

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
-import redis
 import requests
 from bs4 import BeautifulSoup as bs
-from typing import List, Tuple
+from pymongo import MongoClient
+from typing import List, Dict, Tuple
 
 PAGE_URL = "https://bulbapedia.bulbagarden.net/wiki/Category:Illus._by_Yuka_Morii"
 MAILGUN_API_KEY = os.environ["MAILGUN_API_KEY"]
-MAILGUN_DOMAIN = os.environ['MAILGUN_DOMAIN']
+MAILGUN_DOMAIN = os.environ["MAILGUN_DOMAIN"]
 EMAIL_TO = os.environ["MAILGUN_TARGET"]
 EMAIL_FROM = f"yuka-morii-checker@{MAILGUN_DOMAIN}"
-r = redis.from_url(os.environ["REDIS_URL"])
+client = MongoClient(os.environ["MONGO_URL"])
 
 
 def scrape_cards() -> List[Tuple[str, str]]:
@@ -28,47 +28,46 @@ def scrape_cards() -> List[Tuple[str, str]]:
     return cards
 
 
+def get_known_cards() -> Dict[str, str]:
+    return {c["title"]: c["url"] for c in client.ymc.cards.find(projection=["title", "url"])}
+
+
 def filter_new_cards(cards: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
     print("Checking against known cards")
-    new_cards = []
-    for title, url in cards:
-        if r.get(title) is None:
-            print(f" -> Found new: {title}")
-            new_cards.append((title, url))
-        else:
-            print(f" -> Already known: {title}")
+    known_cards = get_known_cards()
+    print(f" -> {len(known_cards.keys())} cards known")
+    new_cards = [c for c in cards if not c[0] in known_cards]
+    for title, _ in new_cards:
+        print(f" -> Found new: {title}")
     return new_cards
 
 
 def save_cards(cards: List[Tuple[str, str]]):
     print("Saving cards")
     for title, url in cards:
-        r.set(title, url)
+        client.ymc.cards.insert_one({"title": title, "url": url})
 
 
 def email_message(cards: List[Tuple[str, str]]):
     card_lines = []
     for card in cards:
         card_lines.append(f"{card[0]} https://bulbapedia.bulbagarden.net{card[1]}")
-    return f"yuka-morii-checker has found {len(cards)} new cards:\n\n" + \
-        "\n".join(card_lines)
+    return f"yuka-morii-checker has found {len(cards)} new cards:\n\n" + "\n".join(card_lines)
 
 
 def send_email(message: str):
     print("Sending email notification")
-    res = requests.post(f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+    requests.post(
+        f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
         auth=("api", MAILGUN_API_KEY),
-        data={
-            "from": EMAIL_FROM,
-            "to": EMAIL_TO,
-            "subject": "yuka morii alert!",
-            "text": message
-        })
+        data={"from": EMAIL_FROM, "to": EMAIL_TO, "subject": "yuka morii alert!", "text": message},
+    )
 
 
 if __name__ == "__main__":
     cards = scrape_cards()
     new_cards = filter_new_cards(cards)
+    print(f"{len(new_cards)} new cards found")
     if len(new_cards) > 0:
-        send_email(email_message(cards))
+        send_email(email_message(new_cards))
         save_cards(new_cards)
